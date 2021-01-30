b134677b1b49499546cb35b795b85f7982e17b72
Peter Kinson <pmkinson@gmail.com>
2021-01-30 16:07:46 -0500

    Viewer coming along

--- src/app/lineage-viewer/view/view.component.ts
+++ src/app/lineage-viewer/view/view.component.ts
@@ -1,19 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
-export class ViewComponent implements OnInit {
 
-  constructor() { }
+export class ViewComponent implements OnInit {
+  @Input() displayLines: Array<string>;
+  @Input() lineageRow: number;
+
+  // Default all values to false
+  private buildList = {
+    'bottom-straight': false,
+    'bottom-left': false,
+    'bottom-right': false,
+    'top-straight': false,
+    'top-left': false,
+    'top-right': false
+  };
+
+  private LINE_DIRECTION = {
+    0: 'bottom-straight',
+    1: 'bottom-left',
+    2: 'bottom-right',
+    3: 'top-straight',
+    4: 'top-left',
+    5: 'top-right',
+  };
+
+  private mapLineDirectionDict(): any[] {
+   return Object.keys(this.LINE_DIRECTION).map(key => {
+        return this.LINE_DIRECTION[key];
+      });
+}
+  constructor() {}
+
+  checkBuildList(direction): boolean {
+    return this.buildList[direction];
+  }
 
   ngOnInit(): void {
+    if (this.lineageRow !== undefined && !(this.lineageRow in [1, 2, 3, 4, 5])) {
+      this.lineageRow = 0;
+    }
+    if (this.displayLines !== undefined) {
+      // Build a list from input directive
+      this.displayLines.forEach(direction => {
+        try {
+          const temp = this.mapLineDirectionDict();
+          this.buildList[direction] = temp.includes(direction);
+        } catch (e) {
+          if (e instanceof TypeError) {
+            const acceptableTypes = this.mapLineDirectionDict();
+            throw TypeError('Acceptable directions for the ViewComponent [displayLines] directive are: \n'
+              .concat(acceptableTypes.join(' ,\n')));
+          } else {
+            throw e;
+          }
+        }
+      });
+    }
   }
 
-  showTopLine(bool): boolean{
-    return bool;
+  isRow(row: number): boolean {
+    return (row === this.lineageRow);
   }
-
 }
